0bbac80aacbc560ea46bf728c66a2d9ec89a26fd
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-12-02 20:10:35 -0300

    adicionando description e footer

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL";
 
 async function converter() {
   let input = document.getElementById("valor").value;
-  let resultado = document.getElementById("resultado")
+  let input1 = document.getElementById('valor1')
   await fetch(url)
     .then((res) => {
       return res.json();
@@ -14,9 +14,55 @@ async function converter() {
       euro = moeda.EURBRL.ask;
     });
 
-    let inputado = Number(input).toFixed(2)
     let dolarFixed = Number(dolar).toFixed(2)
     let euroFixed = Number(euro).toFixed(2)
 
-    resultado.innerHTML = `R$ ${inputado} vale atualmente a ${dolarFixed * input} USD.`
+    let convSelect = document.getElementById('moedaSelect');
+    let convValue = convSelect.options[convSelect.selectedIndex].value
+
+        if (convValue === 'USD') {
+            input1.value = (dolarFixed * input).toFixed(2)
+        } else if (convValue === 'EUR') {
+            input1.value = (euroFixed * input).toFixed(2)
+        }
+}
+
+async function converter1() {
+    let input = document.getElementById("valor1").value;
+    let input1 = document.getElementById('valor')
+    await fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((moeda) => {
+        console.log(moeda);
+  
+        dolar = moeda.USDBRL.ask;
+        euro = moeda.EURBRL.ask;
+      });
+  
+      let dolarFixed = Number(dolar).toFixed(2)
+      let euroFixed = Number(euro).toFixed(2)
+  
+      let convSelect = document.getElementById('moedaSelect');
+      let convValue = convSelect.options[convSelect.selectedIndex].value
+  
+          if (convValue === 'USD') {
+              input1.value = (input / dolarFixed).toFixed(2)
+          } else if (convValue === 'EUR') {
+              input1.value = (input / euroFixed).toFixed(2)
+          }
+
+}
+
+function moedaSel(val) {
+    let input = document.getElementById("valor1")
+    let input1 = document.getElementById('valor')
+    if (val === 'USD') {
+        input.value = ''
+        input1.value = ''
+    } else if (val === 'EUR') {
+        input.value = ''
+        input1.value = ''
+    }
 }
